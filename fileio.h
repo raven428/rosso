d58707a508a8cd84736c06fb3734f7e0be53d1c7
Steven Penny <svnpenn@gmail.com>
2016-06-22 21:49:48 -0500

    fileio.h: indent

--- fileio.h
+++ fileio.h
@@ -1,6 +1,6 @@
 /*
-  This file contains file io functions
-*/
+ * This file contains file io functions
+ */
 
 #ifndef __fileio_h__
 #define __fileio_h__
@@ -10,10 +10,10 @@
 #include <stdint.h>
 #include <windows.h>
 
-FILE* fs_open(char* path, uint32_t mode);
+FILE *fs_open(char *path, uint32_t mode);
 int fs_seek(FILE *stream, off_t offset, int whence);
 off_t fs_read(void *ptr, uint32_t size, uint32_t n, FILE *stream);
 off_t fs_write(const void *ptr, uint32_t size, uint32_t n, FILE *stream);
-int fs_close(FILE* file);
+int fs_close(FILE *file);
 
-#endif  // __fileio_h__
+#endif // __fileio_h__
