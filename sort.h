e38ed3fffec01efc2982d740a234134a1d783237
Steven Penny <svnpenn@gmail.com>
2016-06-22 21:48:12 -0500

    sort.h: indent

--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 /*
-  This file contains/describes functions for sorting of FAT filesystems.
-*/
+ * This file contains/describes functions for sorting of FAT filesystems.
+ */
 
 #ifndef __sort_h__
 #define __sort_h__
@@ -14,9 +14,11 @@
 int32_t sortFileSystem(char *filename);
 
 // sorts directory entries in a cluster
-int32_t sortClusterChain(struct sFileSystem *fs, uint32_t cluster, const char (*path)[MAX_PATH_LEN+1]);
+int32_t sortClusterChain(struct sFileSystem *fs, uint32_t cluster,
+  const char (*path)[MAX_PATH_LEN + 1]);
 
 // returns cluster chain for a given start cluster
-int32_t getClusterChain(struct sFileSystem *fs, uint32_t startCluster, struct sClusterChain *chain);
+int32_t getClusterChain(struct sFileSystem *fs, uint32_t startCluster,
+  struct sClusterChain *chain);
 
 #endif // __sort_h__
