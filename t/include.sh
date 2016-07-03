e25cd27f8f787cd2678e14f70030cf8ba0dafea1
Steven Penny <svnpenn@gmail.com>
2016-07-03 01:40:46 -0500

    make -k INCLUDE=1 2>&1 |

--- t/include.sh
+++ t/include.sh
@@ -1,2 +1,12 @@
 #!/bin/dash -e
-make -k INCLUDE=1
+make -k INCLUDE=1 2>&1 |
+sed '
+/should add these lines/ {
+  s/^/\x1b[1;32m/
+  s/$/\x1b[m/
+}
+/should remove these lines/ {
+  s/^/\x1b[1;31m/
+  s/$/\x1b[m/
+}
+'
