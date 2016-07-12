49f271d5ac8d70eefe4baa19b7d71b1fe19c52d5
Steven Penny <svnpenn@gmail.com>
2016-07-11 21:03:41 -0500

    include.sh: full include-list for

--- t/include.sh
+++ t/include.sh
@@ -1,12 +1,27 @@
 #!/bin/dash -e
+# decorate
+j=$(mktemp | cygpath -wf-)
+for each in *.c *.h
+do
+  printf '#include "%s"\n' "$j" >> "$each"
+done
+
+# transform
 make -k INCLUDE=1 2>&1 |
 sed '
 /should add these lines\|has correct/ {
   s/^/\x1b[1;32m/
-  s/$/\x1b[m/
 }
 /should remove these lines/ {
   s/^/\x1b[1;31m/
-  s/$/\x1b[m/
+}
+/full include-list for/ {
+  s/^/\x1b[1;33m/
+}
+/make\|^$\|---/ {
+  s/^/\x1b[m/
 }
 '
+
+# undecorate
+git checkout *.c *.h
