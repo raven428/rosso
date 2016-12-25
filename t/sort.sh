262d93eb46b66b4ac6267441a384890a4273d490
Steven Penny <svnpenn@gmail.com>
2016-12-24 22:28:34 -0600

    k = split(ARGV[j], q, d); q[1]; for (x in q) printf "%s%s",

--- t/sort.sh
+++ t/sort.sh
@@ -1,9 +1,9 @@
 #!/bin/dash -e
 xc() {
   awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++j < ARGC) {
-  k = split(ARGV[j], q, d); q[1]; for (x in q) {
-  printf q[x] ~ /^[[:alnum:]%+,./:=@_-]+$/ ? q[x] : d q[x] d
-  if (x < k) printf "\\" d} printf j == ARGC - 1 ? "\33[m\n" : FS}}' "$@"
+  k = split(ARGV[j], q, d); q[1]; for (x in q) printf "%s%s",
+  q[x] ~ /^[[:alnum:]%+,./:=@_-]+$/ ? q[x] : d q[x] d, x < k ? "\\" d : ""
+  printf j == ARGC - 1 ? "\33[m\n" : FS}}' "$@"
   "$@"
 }
 
